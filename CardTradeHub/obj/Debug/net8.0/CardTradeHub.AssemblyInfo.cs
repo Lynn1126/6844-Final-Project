28db681d2f7c3d5048148c9d5d8832f15f1f1c1e
Lynn <421666614@qq.com>
2025-04-21 15:47:14 -0700

    final

--- CardTradeHub/obj/Debug/net8.0/CardTradeHub.AssemblyInfo.cs
+++ CardTradeHub/obj/Debug/net8.0/CardTradeHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardTradeHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227cff3c425ca4c2ddd9338d20d1b82bb5bbdfce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f137919acd8e4ef1876976720edbadf4b1576b9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardTradeHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardTradeHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
