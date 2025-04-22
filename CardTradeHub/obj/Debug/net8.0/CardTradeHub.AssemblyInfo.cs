48fcf5353d200530ba609da382850a19d068908c
mulimuli320 <mulimuli320@gmail.com>
2025-04-21 17:57:40 -0700

    card browsing page update

--- CardTradeHub/obj/Debug/net8.0/CardTradeHub.AssemblyInfo.cs
+++ CardTradeHub/obj/Debug/net8.0/CardTradeHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardTradeHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28db681d2f7c3d5048148c9d5d8832f15f1f1c1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda7d5c00a721b617effc98a71c997eb5b04c5b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardTradeHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardTradeHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
